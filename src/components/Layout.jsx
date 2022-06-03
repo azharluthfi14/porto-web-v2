06eddcdb0695d7364666155201fce471398e61cb
azharluthfi14 <35468624+azharluthfi14@users.noreply.github.com>
2022-06-03 14:41:34 +0700

    Setting loader

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,37 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer';
-import SideSocmed from './SideSocmed';
-import Loader from './Loader';
+import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SideSocmed from "./SideSocmed";
+import Loader from "./Loader";
 
 export default function LayoutPage({ children }) {
+  const [isLoading, setLoading] = useState(true);
 
-    const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    isLoading
+      ? document.querySelector("body").classList.add("loading")
+      : document.querySelector("body").classList.remove("loading");
+  }, [isLoading]);
 
-    useEffect(() => {
-        isLoading
-            ? document.querySelector("body").classList.add("loading")
-            : document.querySelector("body").classList.remove("loading");
-    }, [isLoading])
-
-    return (
-        <>
-            {isLoading ? (
-                <Loader setLoading={setLoading} />
-            ) : (
-                <div>
-                    <header>
-                        <Navbar />
-                    </header>
-                    <SideSocmed />
-                    <div className="dark:bg-dark-900">
-                        <main className="mx-8 md:mx-20 lg:mx-40">
-                            {children}
-                            <Footer />
-                        </main>
-                    </div>
-                </div>
-            )}
-        </>
-    )
+  return (
+    <>
+      {isLoading ? (
+        <Loader setLoading={setLoading} />
+      ) : (
+        <div>
+          <header>
+            <Navbar />
+          </header>
+          <SideSocmed />
+          <div className="dark:bg-dark-900">
+            <main className="mx-8 md:mx-20 lg:mx-40">
+              {children}
+              <Footer />
+            </main>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,135 +1,129 @@
-import React, { useState, useEffect } from 'react'
+import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { RiArchiveDrawerFill } from 'react-icons/ri';
 
 export default function Loader({ setLoading }) {
-    // points = "121.9,174.2 121.9,315.5 250,382.2 378.1,315.5 378.1,174.2 "
-    // points = "159.4,295.5 159.4,154.2 250,97.8 339.3,154.2 339.3,295.5 "
-    // x1="250" y1="97.8" x2="250" y2="319.8"
-    const containerAnimation = {
-        visible: {
-            transition: {
-                delayChildren: 0.5,
-                pathLength: {
-                    delay: 0.5,
-                    duration: 1.5,
-                },
-                opacity: {
-                    delay: 0.5,
-                    duration: 0.001
-                },
-            }
-        }
-    };
-
-    const draw = {
-        hidden: { pathLength: 0, opacity: 1 },
-        visible: {
-            pathLength: 1,
-            opacity: 1,
-            fill: ['#ffffff00', '#8b5cf6'],
-            transition: {
-                fill: {
-                    delay: 2.3,
-                    duration: 1,
-                    ease: 'linear'
-                },
-                pathLength: {
-                    delay: 0.5,
-                    duration: 1.5,
-                },
-                opacity: {
-                    delay: 0.5,
-                    duration: 0.001
-                },
-            },
+  const containerAnimation = {
+    hidden: { opacity: 0 },
+    visible: {
+      backgroundColor: "#8b5cf6",
+      opacity: 1,
+      scale: [1, 0],
+      transition: {
+        scale: {
+          delay: 3,
+          duration: 0.5,
+          ease: "backIn",
         },
-    }
-
-    const line = {
-        hidden: {
-            pathLength: 0,
-            opacity: 0,
+        backgroundColor: {
+          delay: 2,
+          duration: 0.7,
+          ease: "circIn",
         },
-        visible: {
-            pathLength: 1,
-            opacity: 1,
-            stroke: ['#8b5cf6', '#ffff'],
-            transition: {
-                pathLength: {
-                    delay: 0.5,
-                    duration: 1.5,
-                },
-                stroke: {
-                    delay: 2.2,
-                    ease: 'linear'
-                },
-                opacity: {
-                    delay: 0.5,
-                    duration: 0.001
-                },
-            }
-        }
-    }
+      },
+    },
+  };
 
-    const fade = {
-        hidden: { opacity: 0, y: 90 },
-        visible: {
-            opacity: 1,
-            y: 0,
-            ease: [0.17, 0.67, 0.83, 0.67],
-            transition: {
-                type: "spring",
-                staggerChildren: 0.5,
-                duration: 1,
-                delay: 1
-            }
-        }
-    }
+  const drawBox = {
+    hidden: { pathLength: 0, opacity: 1 },
+    visible: {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: {
+          delay: 0.5,
+          duration: 1.5,
+          ease: "easeInOut",
+        },
+        opacity: {
+          delay: 0.5,
+          duration: 0.001,
+        },
+      },
+    },
+  };
 
-    return (
-        <div className='h-screen flex justify-center items-center dark:bg-dark-900'>
-            <div className='w-24 h-24'>
-                <AnimatePresence type='crossfade'>
-                    <motion.svg
-                        viewBox="0 0 500 500"
-                        variants={containerAnimation}
-                        initial="hidden"
-                        animate="visible"
-                        onAnimationComplete={() => setLoading(false)}
-                    >
-                        <motion.rect
-                            x="15.2" y="15.2"
-                            width="469.6" height="469.6"
-                            rx={30}
-                            strokeMiterlimit={10}
-                            strokeWidth={20}
-                            stroke="#8b5cf6"
-                            variants={draw}
-                        />
-                        <motion.polyline
-                            fill='none'
-                            rx={30}
-                            stroke="#8b5cf6"
-                            strokeWidth={20}
-                            strokeLinecap="round"
-                            strokeMiterlimit={10}
-                            variants={line}
-                            points="121.9,185.2 121.9,326.5 250,393.2 378.1,326.5 378.1,185.2" />
-                        <motion.polyline
-                            fill='none'
-                            rx={30}
-                            strokeLinecap="round"
-                            stroke="#8b5cf6"
-                            variants={line}
-                            strokeMiterlimit={10}
-                            strokeWidth={20}
-                            points="159.4,304.5 159.4,163.2 250,106.8 339.3,163.2 339.3,304.5" />
-                        <motion.line fill='none' stroke="#8b5cf6" variants={line} strokeWidth={20}
-                            strokeLinecap="round" x1="250" y1="106.8" x2="250" y2="328.8" />
-                    </motion.svg>
-                </AnimatePresence>
-            </div>
-        </div>
-    )
+  const drawLine = {
+    hidden: {
+      pathLength: 0,
+      opacity: 0,
+    },
+    visible: {
+      pathLength: 1,
+      opacity: 1,
+      stroke: ["#8b5cf6", "#ffff"],
+      transition: {
+        pathLength: {
+          delay: 0.5,
+          duration: 1.5,
+        },
+        stroke: {
+          delay: 2.2,
+          ease: "linear",
+        },
+        opacity: {
+          delay: 0.5,
+          duration: 0.001,
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center dark:bg-dark-900">
+      <AnimatePresence type="crossfade">
+        <motion.div
+          variants={containerAnimation}
+          onAnimationComplete={() => setLoading(false)}
+          initial="hidden"
+          animate="visible"
+          className="w-24 h-24 rounded-lg"
+        >
+          <motion.svg fill="none" viewBox="0 0 500 500">
+            <motion.rect
+              x="15.2"
+              y="15.2"
+              variants={drawBox}
+              width="469.6"
+              height="469.6"
+              rx={30}
+              strokeMiterlimit={10}
+              strokeWidth={20}
+              stroke="#8b5cf6"
+            />
+            <motion.polyline
+              fill="none"
+              variants={drawLine}
+              rx={30}
+              stroke="#8b5cf6"
+              strokeWidth={20}
+              strokeLinecap="round"
+              strokeMiterlimit={10}
+              points="121.9,185.2 121.9,326.5 250,393.2 378.1,326.5 378.1,185.2"
+            />
+            <motion.polyline
+              fill="none"
+              variants={drawLine}
+              rx={30}
+              strokeLinecap="round"
+              stroke="#8b5cf6"
+              strokeMiterlimit={10}
+              strokeWidth={20}
+              points="159.4,304.5 159.4,163.2 250,106.8 339.3,163.2 339.3,304.5"
+            />
+            <motion.line
+              fill="none"
+              stroke="#8b5cf6"
+              strokeWidth={20}
+              strokeLinecap="round"
+              x1="250"
+              variants={drawLine}
+              y1="106.8"
+              x2="250"
+              y2="328.8"
+            />
+          </motion.svg>
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 }
